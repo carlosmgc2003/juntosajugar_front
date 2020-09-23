1c924f186e8d047f04eca75cdd1b317af697d747
carlosmgc2003 <carlosmgc2003@gmail.com>
2020-09-23 09:55:25 -0300

    Mostramos el login de usuario en la app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import UserList from "./UserList";
+import UserCreation from "./UserCreation";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <UserList url='http://localhost:4000/user'/>
+          <UserCreation></UserCreation>
       </header>
+
     </div>
   );
 }
