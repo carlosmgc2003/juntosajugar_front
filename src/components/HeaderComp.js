a802d2857100db9f1ea8524769d2152fc663a927
Carlos Maceira <carlosmgc2003@gmail.com>
2020-12-07 10:55:55 -0300

    Feature: la imagen de logo de JAJ ahora es un boton

--- src/components/HeaderComp.js
+++ src/components/HeaderComp.js
@@ -1,6 +1,8 @@
 import React from "react";
 import logo from '../img/juntosajugarlogo.png'
 import Grid from "@material-ui/core/Grid";
+import {Link as RouterLink} from "react-router-dom";
+import {Button} from "@material-ui/core";
 
 
 
@@ -11,10 +13,13 @@ export default function NiceHeader() {
                   alignItems="center">
                 <Grid item>
                     <h1>¡Juntos a Jugar!</h1>
-                    <p>La web donde jugamos y aprendemos :)</p>
+                    <p>La web donde jugamos y aprendemos 😉</p>
                 </Grid>
                 <Grid item>
-                    <img src={logo} alt={'logo'} width="100" height="100"/>
+                    <Button variant="contained" component={RouterLink} to="/">
+                        <img src={logo} alt={'logo'} width="100" height="100"/>
+                    </Button>
+
                 </Grid>
             </Grid>
     );
